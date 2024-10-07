7c6e9f57a1358f0fa8a3ec28471297ece0255edf
Afonso Gomes <afonsogomes.3.vip@gmail.com>
2024-10-07 13:46:29 -0300

    add new option for search top five better ep

--- src/main/java/br/com/datawake/afonso/screenmatch/model/DadosTemporada.java
+++ src/main/java/br/com/datawake/afonso/screenmatch/model/DadosTemporada.java
@@ -1,2 +1,12 @@
-package br.com.datawake.afonso.screenmatch.model;public record DadosTemporada() {
+package br.com.datawake.afonso.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosTemporada(@JsonAlias("Title") String titulo,
+                             @JsonAlias("Season") Integer numeroTemp,
+                             @JsonAlias("Episodes") List<DadosEpisodios> episodios) {
 }
--- src/main/java/br/com/datawake/afonso/screenmatch/principal/Principal.java
+++ src/main/java/br/com/datawake/afonso/screenmatch/principal/Principal.java
@@ -1,2 +1,79 @@
-package br.com.datawake.afonso.screenmatch.principal;public class Principal {
+package br.com.datawake.afonso.screenmatch.principal;
+
+import br.com.datawake.afonso.screenmatch.model.DadosEpisodios;
+import br.com.datawake.afonso.screenmatch.model.DadosSerie;
+import br.com.datawake.afonso.screenmatch.model.DadosTemporada;
+import br.com.datawake.afonso.screenmatch.model.Episodio;
+import br.com.datawake.afonso.screenmatch.service.ConsumoApi;
+import br.com.datawake.afonso.screenmatch.service.ConverteDados;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Principal {
+    private final Scanner sc = new Scanner(System.in);
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=6585022c";
+    private final ConsumoApi consumo = new ConsumoApi();
+    private final ConverteDados conversor = new ConverteDados();
+    public void exibeMenu(){
+        System.out.println("Digite o nome da série que deseja buscar:");
+        var nomeSerie = sc.nextLine();
+        var json = consumo.obterDados(
+                ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        for(int i = 1; i <= dados.totalTemporadas(); i++){
+            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+        temporadas.forEach(System.out::println);
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+//        for(int i = 0; i < dados.totalTemporadas(); i++){
+//            List<DadosEpisodios> episodiosTemporada = temporadas.get(i).episodios();
+//            for (int j = 0; j < episodiosTemporada.size(); j++){
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
+//        }
+
+        List<DadosEpisodios> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("\n Top 5 episódios");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodios::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
+
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .map(d -> new Episodio(t.numeroTemp(), d))
+                ).collect(Collectors.toList());
+        episodios.forEach(System.out::println);
+
+        System.out.println("A partir de que ano gostaria de filtrar??");
+        var ano = sc.nextInt();
+        sc.nextLine();
+
+        LocalDate localDate = LocalDate.of(ano,1,1);
+
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(localDate))
+                .forEach(e -> System.out.println(
+                        "Temporada: " + e.getTemporada() +
+                                " Episodios: " + e.getTitulo() +
+                                " Data lançamento: " + e.getDataLancamento().format(formato)
+                ));
+    }
 }
