3e682b5b45a86b639d1327a4c69da4eb9aa56655
Afonso Gomes <afonsogomes.3.vip@gmail.com>
2024-10-10 14:30:04 -0300

    Projeto final screenmatch 2

---
+++ src/main/java/br/com/datawake/afonso/screenmatch/repository/SerieRepository.java
@@ -0,0 +1,4 @@
+package br.com.datawake.afonso.screenmatch.repository;
+
+public class SerieRepository {
+}
---
+++ src/main/java/br/com/datawake/afonso/screenmatch/service/ConsultaChatGPT.java
@@ -0,0 +1,4 @@
+package br.com.datawake.afonso.screenmatch.service;
+
+public class ComucicacaoChatGPT {
+}
