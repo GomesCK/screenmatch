a273bcc54caef75ca8da074888780586f160aa80
Afonso Gomes <afonsogomes.3.vip@gmail.com>
2024-10-10 14:33:59 -0300

    Projeto final screenmatch 2

--- src/main/java/br/com/datawake/afonso/screenmatch/model/DadosTemporada.java
+++ src/main/java/br/com/datawake/afonso/screenmatch/model/DadosTemporada.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DadosTemporada(@JsonAlias("Title") String titulo,
-                             @JsonAlias("Season") Integer numeroTemp,
+public record DadosTemporada(@JsonAlias("Season") Integer numeroTemp,
                              @JsonAlias("Episodes") List<DadosEpisodios> episodios) {
 }
